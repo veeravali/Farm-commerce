16e92525f0850d3a3f21f06037aaf155091a5ad6
krishna-vvk <krishna.veervalli@gmail.com>
2015-02-24 08:31:16 +0530

    Data for postgres

---
+++ Data-Pg/Database.js
@@ -0,0 +1,36 @@
+﻿(function (database) {
+
+    var pg = require('pg');
+
+var conString = "postgres://postgres:pn121035@PG@localhost:5432/IndiaRTI";
+
+    
+    var theDb = null;
+    database.getdb = function (next) {
+        
+        
+        if (!theDb) {
+
+
+            var client = new pg.Client(conString);
+            client.connect(function(err,db) {
+                
+              if(err) {
+                next(err,null);
+              }
+              else{
+                theDb={
+                    client:db
+                }
+                next(null,theDb);
+              }
+             
+            });
+                       
+        }
+        else {
+            next(null, theDb);
+        }
+    };
+
+})(module.exports);
---
+++ Data-Pg/Index.js
@@ -0,0 +1,64 @@
+﻿(function (data) {
+    
+    var databse = require('./Database.js');
+    data.getLocationValues = function (key,locationType, next) {
+        
+        databse.getdb(function (error, theDb) {
+            debugger;
+            
+            if (error) {
+                console.log("Failed to connect Databse" + error);
+                next(error, null);
+            }
+            else {
+                if(locationType=='S'){
+
+                    theDb.client.query('SELECT state_id,name from places.state', function(err, result) {
+                    if(err) {
+                      return console.error('error running query', err);
+                    }
+
+
+                    console.log(result.rows[0].theTime);
+                    JSON.stringfy(result.rows);
+                    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+                    client.end();
+                    next(null,result.rows);
+                  });
+
+                }   
+                else if(locationType=='D'){
+
+                    client.query('SELECT NOW() AS "theTime"', function(err, result) {
+                    if(err) {
+                      return console.error('error running query', err);
+                    }
+                    console.log(result.rows[0].theTime);
+                    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+                    client.end();
+                  });
+
+                }     
+                else if(locationType=='M'){
+
+
+
+                }
+                else if(locationType=='V'){
+
+                }
+                
+            
+                                
+            }
+
+        });
+       
+        //next(null, seedData.initialNotes);
+    }
+    
+    
+
+  
+
+})(module.exports);
